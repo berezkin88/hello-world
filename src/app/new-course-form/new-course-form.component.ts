2f399f3ba5d744eb9b560ba263eb84b2e1e8c7c4
Alexander <berezkin05@gmail.com>
2020-10-01 23:24:31 +0300

    add new-course-form. currently broken

---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,22 @@
+import { FormArray, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.sass']
+})
+export class NewCourseFormComponent {
+  form;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      name: ['', Validators.required],
+      contact: fb.group({
+        email: [],
+        phone: []
+      }),
+      topics: fb.array([])
+    });
+  }
+}
